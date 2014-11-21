e383543fe3b9e94b8abce2dbd31469bd9a2d348c
StephHippo <stephaniehippo@gmail.com>
2014-11-21 00:30:27 -0500

    Specifying location of forms and models in the view because windows.

--- pamlla/views.py
+++ pamlla/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import Context, loader
-from forms import NewPatientForm
-from models import Patient, User, Doctor, Prediction, Logit, HazardFunction, MutatedGenes, SurvivalFactors
+from pamlla.forms import NewPatientForm
+from pamlla.models import Patient, User, Doctor, Prediction, Logit, HazardFunction, MutatedGenes, SurvivalFactors
 # Create your views here.
 
 
