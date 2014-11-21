bfc0a2ffd76c644a2840004c25dd59b04a7188b4
StephHippo <stephaniehippo@gmail.com>
2014-11-21 00:05:48 -0500

    Adding labels?

--- pamlla/forms.py
+++ pamlla/forms.py
@@ -2,6 +2,6 @@ __author__ = 'stephaniehippo'
 from django import forms
 
 class NewPatientForm(forms.Form):
-    username = forms.CharField()
-    patient_name = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(label="User Name")
+    patient_name = forms.CharField(label="Patient Name")
+    password = forms.CharField(widget=forms.PasswordInput(), label="Password")
