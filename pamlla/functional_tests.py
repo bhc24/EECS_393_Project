23fd18dbd4880a13cf3a82357222c42f9877f896
StephHippo <stephaniehippo@gmail.com>
2014-11-21 03:42:05 -0500

    Functional tests work, but a little jank.

--- pamlla/functional_tests.py
+++ pamlla/functional_tests.py
@@ -1,8 +1,17 @@
 __author__ = 'stephaniehippo'
 
 from selenium import webdriver
+from django.test import TestCase
 
 browser = webdriver.Firefox()
 browser.get('http://localhost:8000/home/')
 
+#confirms page loads
 assert 'PAMLLA Login' in browser.title
+
+browser.get('http://localhost:8000/patients/')
+
+table = browser.find_element_by_id("patient_list")
+assert len(table.find_elements_by_tag_name('tr')) >= 2
+
+browser.close()
