f211545e92067d9e2efc4bd4c8fbe5b59e277bb8
bhc24 <bhc24@case.edu>
2014-11-21 04:09:46 -0500

    Recovering conflicts.

--- Scripts/Logistic_Regression.py
+++ Scripts/Logistic_Regression.py
@@ -1,37 +1,3 @@
 __author__ = 'Ben'
 
-import numpy as np
-import pandas as pd
-import statsmodels.api as sm
-import pylab as pl
-
-
-df = pd.read_csv("http://www.ats.ucla.edu/stat/data/binary.csv")
-
-print(df.head())
-print()
-print()
-
-df.columns = ["admit", "gre", "gpa", "prestige"]
-
-dummy_ranks = pd.get_dummies(df['prestige'], prefix='prestige')
-
-cols_to_keep = ['admit', 'gre', 'gpa']
-
-data = df[cols_to_keep].join(dummy_ranks.ix[:, 'prestige_2':])
-
-data['intercept'] = 1.0
-
-train_cols = data.columns[1:]
-
-logit = sm.Logit(data['admit'], data[train_cols])
-
-result = logit.fit()
-
-print(result.summary())
-print()
-print()
-
-print(np.exp(result.params))
-
 
--- Scripts/Parser.py
+++ Scripts/Parser.py
@@ -2,6 +2,8 @@ __author__ = 'Ben'
 
 
 import re
+import pickle
+import pandas as pd
 
 
 
@@ -77,6 +79,34 @@ def mutation():
             mutated_gene.append(gene)
 
 
+def mutation2():
+    patient_gene = {}
+
+    with open('mutation.maf', 'r') as file:
+        line_count = 0
+
+        for line in file:
+
+            if(line_count == 0):
+                line_count += 1
+                continue
+
+            temp = line.split()
+
+            if('Silent' in temp[8]):
+                continue
+            else:
+                gene = temp[0]
+                patient = temp[15].split('-')[2]
+
+            if patient not in patient_gene:
+                patient_gene.setdefault(patient, []).append(gene)
+            elif gene not in patient_gene[patient]:
+                patient_gene[patient].append(gene)
+
+
+
+
 def clinical():
 
     with open('clinical.txt' 'r') as file:
@@ -95,3 +125,40 @@ def clinical():
                 time = temp[17]
             else:
                 time = temp[16]
+
+
+
+def clinical2():
+    patient_clinical = {}
+
+    with open('clinical.txt', 'r') as file:
+        for line in file:
+
+            temp = line.split('\t')
+            pat_id = temp[0].split('-')[2]
+            status = temp[1]
+
+            if 'Dead' in status and '[Not Available]' not in temp[3]:
+                try:
+                    time = int(temp[3].rstrip())
+                    patient_clinical.setdefault(pat_id, []).append(time)
+                    patient_clinical[pat_id].append(0)
+                except AttributeError:
+                    print(pat_id)
+                    print(status)
+            elif 'Alive' in status and '[Not Available]' not in temp[2]:
+                time = int(temp[2].rstrip())
+                patient_clinical.setdefault(pat_id, []).append(time)
+                patient_clinical[pat_id].append(1)
+            else:
+                continue
+
+
+
+    fp = open('clinical.pkl', 'wb')
+    pickle.dump(patient_clinical, fp)
+    fp.close()
+
+
+clinical2()
+
