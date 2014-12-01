6a03a2f54c9e9642388745e7de1dfb5820fa8b83
StephHippo <stephaniehippo@gmail.com>
2014-11-30 22:08:33 -0500

    Changing url view calls

--- pamlla_site/urls.py
+++ pamlla_site/urls.py
@@ -1,20 +1,21 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from pamlla.views import patients, add_patient, login_view, home, history, signup_view
+from pamlla import views
 
 
 admin.autodiscover()
 
+#TODO: Pass patient arguments
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pamlla_site.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^patient_list/', patients),
-    url(r'^add_patient/', add_patient),
-    url(r'^login/', login_view),
-    url(r'^home/', patients),
-    url(r'^history/', history),
-    url(r'^signup/', signup_view),
-    url(r'^$', login_view)
+    url(r'^patient_list/', views.patients),
+    url(r'^add_patient/', views.add_patient),
+    url(r'^login/', views.login_view),
+    url(r'^home/', views.patients),
+    url(r'^history/', views.history),
+    url(r'^signup/', views.signup_view),
+    url(r'^$', views.login_view)
 )
