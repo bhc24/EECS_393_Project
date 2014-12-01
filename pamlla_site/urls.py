fb81cbfd9bcce37f11553c2897458e98a59e0f76
bhc24 <bhc24@case.edu>
2014-11-30 23:24:55 -0500

    Try to go 10 minutes without breaking anything

--- pamlla_site/urls.py
+++ pamlla_site/urls.py
@@ -17,9 +17,6 @@ urlpatterns = patterns('',
     url(r'^home/', views.patients),
     url(r'^history/', views.history),
     url(r'^signup/', views.register),
-<<<<<<< HEAD
     url(r'^logout/', views.logout_view),
-=======
->>>>>>> bde0fe8182eed6506c3ac0e626babde616e51533
     url(r'^$', views.login_view)
 )
